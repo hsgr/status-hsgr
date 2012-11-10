9e06dd8f888ce93fb260bea63e2bf0cae1cc74d4
Nikos Roussos <nikos@roussos.cc>
2012-11-10 19:11:48 +0200

    added ical parser

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,9 +18,40 @@ $.ajax({
 });
 };
 
+function get_events() {
+  var ical_file = '/hsgr.ics';
+
+  //run ical parser on load
+  window.addEventListener("load", function() {
+    //Create new ical parser
+    new ical_parser(ical_file, function(cal) {
+      //When ical parser has loaded file
+      //get future events
+      events = cal.getFutureEvents();
+      //And display them
+      displayDemo(events);
+    });
+  }, true);
+
+  //Display All future events in ical file as list.
+  function displayDemo(events) {
+    //Foreach event
+    events.forEach(function(event) {
+      //Create a list item
+      var li = document.createElement('li');
+      //Add details from cal file.
+      li.innerHTML = '<strong>' +event.SUMMARY + '</strong><br/> ' +
+      event.day + ': ' +event.start_time + ' - ' + event.end_time + ' ('+event.start_date+ ')' ;
+      //Add list item to list.
+      document.getElementById('calendar').appendChild(li);
+    });
+  }
+}
+
 $(document).ready(function() {
   $('#loading').toggle();
   get_counter();
+  get_events();
   var refreshId = setInterval(function() {
     get_counter();
   }, 100000);
