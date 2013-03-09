02beeccb85dfda1cf0fb200b7f9e792b886ac956
Nikos Roussos <nikos@roussos.cc>
2013-03-09 21:48:24 +0200

    fix identation

--- javascripts/app.js
+++ javascripts/app.js
@@ -53,22 +53,22 @@ $(document).ready(function() {
 
 //get hackers counter
 function get_counter() {
-$.ajax({
-  url: 'http://hackerspace.gr/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Network/Leases',
-  dataType: 'jsonp',
-  crossDomain: true,
-  cache: false
-}).done(function(json) {
-  var count = json.query.pages[168].revisions[0]["*"];
-  $('#counter').html(count);
-  if (count == "0") {
-    $('.counter.panel').attr("id","close");
-    $('#openornot').html('hackers in space, means that space is now closed!');
-  } else {
-    $('.counter.panel').attr("id","open");
-    $('#openornot').html('hackers in space, means that space is now open!');
-  }
-});
+  $.ajax({
+    url: 'http://hackerspace.gr/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Network/Leases',
+    dataType: 'jsonp',
+    crossDomain: true,
+    cache: false
+  }).done(function(json) {
+    var count = json.query.pages[168].revisions[0]["*"];
+    $('#counter').html(count);
+    if (count == "0") {
+      $('.counter.panel').attr("id","close");
+      $('#openornot').html('hackers in space, means that space is now closed!');
+    } else {
+      $('.counter.panel').attr("id","open");
+      $('#openornot').html('hackers in space, means that space is now open!');
+    }
+  });
 };
 
 //Display All future events in ical file as list.
